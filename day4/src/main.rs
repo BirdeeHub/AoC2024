538a8a5dff531699f1571f9f2dea283492621a0b
BirdeeHub <birdee@localhost>
2024-12-04 17:17:19 -0800

    part2

--- day4/src/main.rs
+++ day4/src/main.rs
@@ -7,16 +7,24 @@ fn main() -> io::Result<()> {
     let file = File::open("input")?;
     let reader = BufReader::new(file);
 
-    let mut wordsearch:Vec<Vec<char>> = Vec::new();
+    let mut board:Vec<Vec<char>> = Vec::new();
 
     for line in reader.lines() {
         let line = line?;
-        wordsearch.push(line.chars().collect());
+        board.push(line.chars().collect());
     }
 
-    let rows = wordsearch.len();
-    let cols = wordsearch[0].len();
+    let rows = board.len();
+    let cols = board[0].len();
+    let target = ('M' as u32) + ('S' as u32);
     let mut xmas_count = 0;
+    for x in 1..rows-1 {
+        for y in 1..cols-1 {
+            if board[x][y] == 'A' && (board[x-1][y-1] as u32) + (board[x+1][y+1] as u32) == target && (board[x+1][y-1] as u32) + (board[x-1][y+1] as u32) == target {
+                xmas_count += 1;
+            }
+        }
+    }
 
     println!("total XMAS: {}", xmas_count);
     println!("Time taken: {:?}", start.elapsed());
