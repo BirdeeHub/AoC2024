9a45ed9ca71ae6317bb64cbf00b67066ccc058db
BirdeeHub <birdee@localhost>
2024-12-06 01:28:19 -0800

    this is how we generate our shit.
    (auto-msg whatthecommit.com)
    
    On branch master
    Your branch is up to date with 'origin/master'.
    
    Changes to be committed:
      (use "git restore --staged <file>..." to unstage)
    	modified:   src/main.rs
    	modified:   src/part1.rs
    	modified:   src/part2.rs
    	new file:   src/types.rs

--- day06/src/main.rs
+++ day06/src/main.rs
@@ -1,6 +1,7 @@
 use std::io::Result;
 mod part1;
 mod part2;
+mod types;
 fn main() -> Result<()> {
     part1::run()?;
     part2::run()
---
+++ day06/src/types.rs
@@ -0,0 +1,54 @@
+use std::fmt::{Display, Formatter};
+
+#[derive(Debug, PartialEq, Clone)]
+pub enum Direction {
+    Up,
+    Down,
+    Left,
+    Right,
+}
+
+#[derive(Debug, PartialEq, Clone)]
+pub enum RoomSpace {
+    Guard(Direction),
+    Obstacle,
+    Visited,
+    Empty,
+}
+
+impl Display for RoomSpace {
+    fn fmt(&self, fmt:&mut Formatter) -> Result<(), std::fmt::Error> {
+        fmt.write_str(match self {
+            RoomSpace::Guard(dir) => match dir {
+                Direction::Up => "^",
+                Direction::Down => "v",
+                Direction::Left => "<",
+                Direction::Right => ">",
+            },
+            RoomSpace::Obstacle => "#",
+            RoomSpace::Visited => ".",
+            RoomSpace::Empty => " ",
+        })
+    }
+}
+use std::time::Duration;
+use std::thread;
+pub fn print_room(room: &[Vec<RoomSpace>]) {
+    thread::sleep(Duration::from_millis(250));
+    println!("{}","-".repeat(room[0].len()));
+    if room.is_empty() {
+        return;
+    }
+
+    let num_cols = room.len();
+    let num_rows = room[0].len();
+
+    for col in 0..num_rows {
+        let row: String = (0..num_cols)
+            .map(|row| room[row][col].to_string())
+            .collect();
+        println!("{}", row);
+    }
+    println!("{}","-".repeat(room[0].len()));
+}
+
