0951822ea6a959cbac3a66ff2bd97c725cfdf25b
BirdeeHub <birdee@localhost>
2024-12-14 11:05:17 -0800

    This is why the cat shouldn't sit on my keyboard.
    (auto-msg whatthecommit.com)
    
    On branch master
    Your branch is up to date with 'origin/master'.
    
    Changes to be committed:
      (use "git restore --staged <file>..." to unstage)
    	modified:   src/part2.rs

--- day09/src/part2.rs
+++ day09/src/part2.rs
@@ -9,6 +9,12 @@ fn read_file(file_path: &str) -> io::Result<String> {
     Ok(contents)
 }
 
+#[derive(Debug, Clone, Copy)]
+enum Segment {
+    Block(usize,usize),
+    Empty(usize),
+}
+
 pub fn run() -> io::Result<()> {
     let start = Instant::now();
     let args: Vec<String> = std::env::args().collect();
@@ -17,29 +23,87 @@ pub fn run() -> io::Result<()> {
         _ => env::var("AOC_INPUT").expect("AOC_INPUT not set")
     };
 
-    let mut disk: Vec<Option<u64>> = Vec::new();
-    let input: Vec<u64> = read_file(&filepath)?.trim().chars().map(|c| c.to_digit(10).unwrap() as u64).collect();
+    let mut disk: Vec<Segment> = Vec::new();
+    let input: Vec<usize> = read_file(&filepath)?.trim().chars().map(|c| c.to_digit(10).unwrap() as usize).collect();
     let mut segment_num = 0;
     let mut i = 0;
     while i < input.len() {
         if let Some(num) = input.get(i) {
-            if *num > 0 {
-                for _ in 0..*num {
-                    disk.push(Some(segment_num));
-                }
-                segment_num += 1;
-            }
+            disk.push(Segment::Block(segment_num,*num));
+            segment_num += 1;
             if let Some(spaces) = input.get(i+1) {
-                for _ in 0..*spaces {
-                    disk.push(None);
+                if *spaces > 0 {
+                    disk.push(Segment::Empty(*spaces));
                 }
             }
         }
         i += 2;
     }
-    while disk.last().is_none() {
+    while let Some(Segment::Empty(_)) = disk.last() {
         disk.pop();
     }
+    println!("{:?}",disk);
+    let mut revdisk: Vec<Segment> = disk.clone();
+    revdisk.reverse();
+    for (i, segment) in revdisk.iter().enumerate() {
+        let mut to_add: Vec<(usize,Segment)> = Vec::new();
+        match segment {
+            Segment::Empty(_) => {
+                continue;
+            },
+            Segment::Block(num,size) => {
+                for (idx, val) in disk.iter_mut().enumerate() {
+                    if idx >= revdisk.len()-1-i { break; };
+                    if let Segment::Empty(width) = val {
+                        match width {
+                            _ if *width > *size => {
+                                *val = Segment::Empty(*width-size);
+                                to_add.push((idx,Segment::Block(*num,*size)));
+                                break;
+                            },
+                            _ if *width == *size => {
+                                *val = Segment::Block(*num,*size);
+                                break;
+                            },
+                            _ => { continue; },
+                        }
+                    }
+                }
+            }
+        }
+        for (idx,block) in to_add.iter().rev() {
+            disk.insert(*idx, *block);
+        }
+    }
+    //TODO:
+    // You need to pop the old ones off of disk after moving them
+
+    let mut xpanddisk: Vec<Option<usize>> = Vec::new();
+    for segment in disk.iter() {
+        match segment {
+            Segment::Block(num,size) => {
+                for _ in 0..*size {
+                    xpanddisk.push(Some(*num));
+                }
+            },
+            Segment::Empty(size) => {
+                for _ in 0..*size {
+                    xpanddisk.push(None);
+                }
+            },
+        }
+    }
+
+    println!("{:?}",xpanddisk);
+
+    let mut checksum: u64 = 0;
+    for (i, val) in xpanddisk.iter().enumerate() {
+        if let Some(num) = val {
+            checksum += *num as u64 * (i as u64);
+        }
+    }
+
+    println!("{:?}",checksum);
 
     println!("Time taken: {:?}", start.elapsed());
 
