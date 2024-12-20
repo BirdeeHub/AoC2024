7d56569bb63c128e1275618336541e332bdf6a8c
BirdeeHub <birdee@localhost>
2024-12-19 21:50:49 -0800

    Whatever will be, will be 8{
    (auto-msg whatthecommit.com)
    
    On branch master
    Your branch is up to date with 'origin/master'.
    
    Changes to be committed:
      (use "git restore --staged <file>..." to unstage)
    	modified:   src/part1.rs

--- day10/src/part1.rs
+++ day10/src/part1.rs
@@ -3,6 +3,17 @@ use std::time::Instant;
 use std::io::{self, BufRead, BufReader};
 use std::env;
 
+struct Position {
+    row: usize,
+    col: usize
+}
+
+impl Position {
+    fn new(row: usize, col: usize) -> Position {
+        Position { col, row }
+    }
+}
+
 pub fn run() -> io::Result<()> {
     let start = Instant::now();
     let filepath = match std::env::args().collect::<Vec<String>>().get(1) {
@@ -12,16 +23,19 @@ pub fn run() -> io::Result<()> {
     let file = File::open(filepath)?;
     let reader = BufReader::new(file);
 
-    let mut map:Vec<Vec<usize>> = Vec::new();
-
+    let mut map = Vec::new();
     for line in reader.lines() {
         let line = line?;
         let row:Vec<usize> = line.chars().map(|c| c.to_digit(10).unwrap() as usize).collect();
         map.push(row);
     }
-
-    for row in map.iter() {
-        println!("{:?}", row);
+    let mut trailheads = Vec::new();
+    for (i,row) in map.iter().enumerate() {
+        for (j, cell) in row.iter().enumerate() {
+            if *cell == 0 {
+                trailheads.push(Position::new(i,j));
+            }
+        }
     }
 
     println!("Time taken: {:?}", start.elapsed());
