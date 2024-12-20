743f193fd635ead44ae0785d7df36f0488503a48
BirdeeHub <birdee@localhost>
2024-12-20 05:08:41 -0800

    touched...
    (auto-msg whatthecommit.com)
    
    On branch master
    Your branch is up to date with 'origin/master'.
    
    Changes to be committed:
      (use "git restore --staged <file>..." to unstage)
    	modified:   src/part2.rs

--- day11/src/part2.rs
+++ day11/src/part2.rs
@@ -1,4 +1,5 @@
 use std::env;
+use std::collections::HashMap;
 use std::fs::File;
 use std::io::{self, Read};
 use std::time::Instant;
@@ -16,37 +17,40 @@ pub fn run() -> io::Result<()> {
         Some(fp) => fp.to_string(),
         _ => env::var("AOC_INPUT").expect("AOC_INPUT not set"),
     };
-    let mut stones: Vec<u64> = read_file(&filepath)?
+    let stones: Vec<u64> = read_file(&filepath)?
         .split_whitespace()
         .map(|v| v.parse::<u64>().unwrap())
         .collect();
 
-    for i in 0..75 {
-        println!("blink {}", i+1);
-        do_blink(&mut stones);
-    }
-    println!("Part 2, 75 blinks: {}", stones.len());
+    let mut memo: HashMap<(u64,u64), u64> = HashMap::new();
+
+    let res = stones.iter().map(|v| count(*v, 75, &mut memo)).sum::<u64>();
+
+    println!("Part 2, 25 blinks: {}", res);
 
     println!("Time taken: {:?}", start.elapsed());
 
     Ok(())
 }
 
-fn do_blink(stones: &mut Vec<u64>) {
-    let stlen = stones.len();
-    for i in 0..stlen {
-        let v = stones[i];
-        if v == 0 {
-            stones[i] = 1;
+fn count(stone: u64, steps: u64, memo: &mut HashMap<(u64,u64), u64>) -> u64 {
+    if let Some(&res) = memo.get(&(stone, steps)) {
+        return res;
+    }
+    let ret;
+    if steps == 0 {
+        ret = 1;
+    } else if stone == 0 {
+        ret = count(1, steps - 1, memo);
+    } else {
+        let numlen = (stone as f64).log10().floor() as u64 + 1;
+        if numlen % 2 == 0 {
+            let divisor = 10u64.pow((numlen / 2) as u32);
+            ret = count(stone / divisor, steps - 1, memo) + count(stone % divisor, steps - 1, memo);
         } else {
-            let numlen = (v as f64).log10().floor() as u64 + 1;
-            if numlen % 2 == 0 {
-                let divisor = 10u64.pow((numlen / 2) as u32);
-                stones[i] = v / divisor;
-                stones.push(v % divisor);
-            } else {
-                stones[i] = v * 2024;
-            };
+            ret = count(stone * 2024, steps - 1, memo);
         };
-    };
+    }
+    memo.insert((stone, steps), ret);
+    ret
 }
