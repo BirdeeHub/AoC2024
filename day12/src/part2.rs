5205dc2730556c1a204d7b5ff414cb8da948a014
BirdeeHub <birdee@localhost>
2024-12-20 13:05:04 -0800

    this is Spartaaaaaaaa
    (auto-msg whatthecommit.com)
    
    On branch master
    Your branch is up to date with 'origin/master'.
    
    Changes to be committed:
      (use "git restore --staged <file>..." to unstage)
    	modified:   src/part2.rs

--- day12/src/part2.rs
+++ day12/src/part2.rs
@@ -77,7 +77,8 @@ impl Region {
     fn calc_cost(&self) -> u64 {
         // perimeter * area
         self.iter().fold(0, |acc, plot| acc + (plot.edges as u64)) * (self.len() as u64)
-        // TODO: fix for part 2 so that its contiguous sides * area instead
+        // TODO: fix for part 2 so that its sides * area instead
+        // where sides are contiguous edges that are on the same line
     }
 }
 
