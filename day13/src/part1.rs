153aa0cb5915bae0a9950c2a00470e679f897ca7
BirdeeHub <birdee@localhost>
2024-12-24 05:53:19 -0800

    assorted changes
    (auto-msg whatthecommit.com)
    
    On branch master
    Your branch is up to date with 'origin/master'.
    
    Changes to be committed:
      (use "git restore --staged <file>..." to unstage)
    	modified:   src/part1.rs

--- day13/src/part1.rs
+++ day13/src/part1.rs
@@ -25,7 +25,7 @@ pub fn run() -> io::Result<()> {
     })?;
     let reader = BufReader::new(file);
 
-    let button_match = Regex::new(r"^Button (A|B): X\+(\d+), Y\+(\d+)$").unwrap();
+    let button_match = Regex::new(r"^Button A|B: X\+(\d+), Y\+(\d+)$").unwrap();
     let prize_match = Regex::new(r"^Prize: X=(\d+), Y=(\d+)$").unwrap();
 
     let mut results = Vec::new();
@@ -34,35 +34,46 @@ pub fn run() -> io::Result<()> {
         if ! line.is_empty() {
             if button_match.is_match(&line) {
                 for (_, [id,dx,dy]) in button_match.captures_iter(&line).map(|c| c.extract()) {
-                    results.push((id.to_string(), dx.parse::<i32>().unwrap(), dy.parse::<i32>().unwrap()));
+                    results.push((dx.parse::<i32>().unwrap(), dy.parse::<i32>().unwrap()));
                 }
             } else if prize_match.is_match(&line) {
                 for (_, [x,y]) in prize_match.captures_iter(&line).map(|c| c.extract()) {
-                    results.push(("P".to_string(), x.parse::<i32>().unwrap(), y.parse::<i32>().unwrap()));
+                    results.push((x.parse::<i32>().unwrap(), y.parse::<i32>().unwrap()));
                 }
             }
         }
     }
 
-    let machines:Vec<HashMap<String,Point>> = results.chunks(3).map(|chunk| {
-        let mut res = HashMap::new();
-        for (id,x,y) in chunk {
-            res.insert(id.clone(), Point::new(*x, *y));
+    let machines:Vec<[Point;3]> = results.chunks(3).map(|chunk| {
+        let mut res = Vec::new();
+        for (x,y) in chunk {
+            res.push(Point::new(*x, *y));
         }
-        res
+        match (&res[0..2]).try_into() {
+            Ok(arr) => arr,
+            Err(_) => panic!("AOC input invalid, not all machines have 2 buttons and a prize")
+        }
     }).collect();
 
-    for m in machines {
-        // TODO: Your algebra sucked and you didnt even read the whole problem...
-        // A costs 3 and B costs 1
-        // you didnt even solve for the right thing nor did you do it right...
-        let canReach = (m["P"].y * m["A"].x - m["P"].x * m["A"].y) % (m["P"].y * m["B"].x - m["P"].x * m["B"].y) == 0;
-        let Btimes = (m["P"].y * m["A"].x - m["P"].x * m["A"].y) / (m["P"].y * m["B"].x - m["P"].x * m["B"].y);
-        let Atimes = (m["B"].x - m["P"].x * Btimes) / m["A"].x;
-        println!("{} {} {}", Atimes, Btimes, canReach);
+    let mut total = 0;
+    for [a, b, p] in machines {
+        total += solve(a,b,p).unwrap_or(0);
     }
+
+    println!("Part 1: {}", total);
 
     println!("Time taken: {:?}", start.elapsed());
 
     Ok(())
+}
+
+// returns token count or none
+fn solve(a: Point, b: Point, p: Point) -> Option<(usize)> {
+    // TODO: Your algebra sucked and you didnt even read the whole problem...
+    // A costs 3 and B costs 1
+    // you didnt even solve for the right thing nor did you do it right...
+    let canReach = (p.y * a.x - p.x * a.y) % (p.y * b.x - p.x * b.y) == 0;
+    let Btimes = (p.y * a.x - p.x * a.y) / (p.y * b.x - p.x * b.y);
+    let Atimes = (b.x - p.x * Btimes) / a.x;
+    println!("{} {} {}", Atimes, Btimes, canReach);
 }
