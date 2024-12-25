522679d5d3497e269330bd4d32135720bcb6f7ad
BirdeeHub <birdee@localhost>
2024-12-25 04:54:20 -0800

    lots of changes after a lot of time
    (auto-msg whatthecommit.com)
    
    On branch master
    Your branch is up to date with 'origin/master'.
    
    Changes to be committed:
      (use "git restore --staged <file>..." to unstage)
    	new file:   Cargo.lock
    	new file:   Cargo.toml
    	new file:   input1
    	new file:   src/main.rs
    	new file:   src/part1.rs

---
+++ day14/src/part1.rs
@@ -0,0 +1,48 @@
+use std::fs::File;
+use std::time::Instant;
+use std::io::{self, BufRead, BufReader};
+use std::env;
+use regex::Regex;
+
+#[derive(Debug, Copy, Clone)]
+struct Vec2 {
+    x: i32,
+    y: i32
+}
+impl Vec2 {
+    fn new(x: i32, y: i32) -> Vec2 {
+        Vec2{x, y}
+    }
+}
+
+pub fn run() -> io::Result<()> {
+    let start = Instant::now();
+    let args: Vec<String> = std::env::args().collect();
+    let file = File::open(match args.get(1) {
+        Some(fp) => fp.to_string(),
+        _ => env::var("AOC_INPUT").expect("AOC_INPUT not set"),
+    })?;
+    let reader = BufReader::new(file);
+
+    let bot_match = Regex::new(r"^p\=(\d+),(\d+) v\=(-*\d+),(-*\d+)$").unwrap();
+
+    let mut results = Vec::new();
+    for line in reader.lines() {
+        let line = line?;
+        if ! line.is_empty() && bot_match.is_match(&line) {
+            for (_, [x,y,dx,dy]) in bot_match.captures_iter(&line).map(|c| c.extract()) {
+                let p = Vec2::new(x.parse().unwrap(), y.parse().unwrap());
+                let v = Vec2::new(dx.parse().unwrap(), dy.parse().unwrap());
+                results.push((p,v));
+            }
+        }
+    }
+
+    for (p,v) in &results {
+        println!("{:?}: {:?}", p, v);
+    }
+
+    println!("Time taken: {:?}", start.elapsed());
+
+    Ok(())
+}
