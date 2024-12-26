de3fc862e32c8b00821fe4b8298a6ba7affaa2d9
BirdeeHub <birdee@localhost>
2024-12-26 02:11:33 -0800

    I only play the games that I win at.
    (auto-msg whatthecommit.com)
    
    On branch master
    Your branch is up to date with 'origin/master'.
    
    Changes to be committed:
      (use "git restore --staged <file>..." to unstage)
    	modified:   src/part1.rs

--- day14/src/part1.rs
+++ day14/src/part1.rs
@@ -2,6 +2,7 @@ use std::fs::File;
 use std::time::Instant;
 use std::io::{self, BufRead, BufReader};
 use std::env;
+use std::ops::Add;
 use regex::Regex;
 
 #[derive(Debug, Copy, Clone)]
@@ -12,6 +13,33 @@ struct Vec2 {
 impl Vec2 {
     fn new(x: i32, y: i32) -> Vec2 {
         Vec2{x, y}
+    }
+}
+impl Add for Vec2 {
+    type Output = Vec2;
+
+    fn add(self, other: Vec2) -> Vec2 {
+        Vec2 {
+            x: self.x + other.x,
+            y: self.y + other.y,
+        }
+    }
+}
+
+#[derive(Debug, Clone)]
+struct Bot {
+    p: Vec2,
+    v: Vec2
+}
+impl Bot {
+    fn move_bot(&mut self, w: i32, h: i32) {
+        // get new_p, if its more, get that with mod, add max to shift it
+        // then use mod again to limit it and wrap again if still bigger.
+        let new_p = self.p + self.v;
+        self.p = Vec2 {
+            x: (new_p.x % w + w) % w,
+            y: (new_p.y % h + h) % h,
+        };
     }
 }
 
@@ -32,16 +60,36 @@ pub fn run() -> io::Result<()> {
             for (_, [x,y,dx,dy]) in bot_match.captures_iter(&line).map(|c| c.extract()) {
                 let p = Vec2::new(x.parse().unwrap(), y.parse().unwrap());
                 let v = Vec2::new(dx.parse().unwrap(), dy.parse().unwrap());
-                bots.push((p,v));
+                bots.push(Bot{p,v});
             }
         }
     }
 
-    for (p,v) in &bots {
-        println!("{:?}: {:?}", p, v);
+    let room_w = 11;
+    let room_h = 7;
+
+    for bot in &mut bots {
+        for _ in 0..100 {
+            bot.move_bot(room_w as i32, room_h as i32);
+        }
     }
+    let mut room = vec![vec![0; room_w]; room_h];
+    for bot in bots {
+        room[bot.p.y as usize][bot.p.x as usize] += 1;
+    }
+
+    print_room(&room);
 
     println!("Time taken: {:?}", start.elapsed());
 
     Ok(())
+}
+
+fn print_room(room: &[Vec<usize>]) {
+    for row in room {
+        for val in row {
+            print!("{}", val);
+        }
+        println!();
+    }
 }
