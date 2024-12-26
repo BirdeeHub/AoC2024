39bf7a9eef41a8f3b10008f0c15776508c2a1f54
BirdeeHub <birdee@localhost>
2024-12-26 06:07:11 -0800

    I know what I am doing. Trust me.
    (auto-msg whatthecommit.com)
    
    On branch master
    Your branch is up to date with 'origin/master'.
    
    Changes to be committed:
      (use "git restore --staged <file>..." to unstage)
    	new file:   day15/Cargo.lock
    	new file:   day15/Cargo.toml
    	new file:   day15/input1
    	new file:   day15/src/main.rs
    	new file:   day15/src/part1.rs

---
+++ day15/src/part1.rs
@@ -0,0 +1,48 @@
+use std::fs::File;
+use std::time::Instant;
+use std::io::{self, Read};
+use std::env;
+
+#[derive(Debug,Clone,Copy)]
+enum Move {
+    L,
+    R,
+    U,
+    D,
+}
+
+fn read_file(file_path: &str) -> io::Result<String> {
+    let mut contents = String::new();
+    File::open(file_path)?.read_to_string(&mut contents)?;
+    Ok(contents)
+}
+
+pub fn run() -> io::Result<()> {
+    let start = Instant::now();
+    let args: Vec<String> = std::env::args().collect();
+    let filepath = match args.get(1) {
+        Some(fp) => fp.to_string(),
+        _ => env::var("AOC_INPUT").expect("AOC_INPUT not set")
+    };
+    let contents = read_file(&filepath).unwrap();
+    let inparts:Vec<&str> = contents.split("\n\n").collect();
+    let mapstr = inparts[0];
+    let map:Vec<Vec<char>> = Vec::new();
+    for line in mapstr.lines() {
+    }
+    let movestr = inparts[1];
+    let mut moves = Vec::new();
+    for c in movestr.chars() {
+        match c {
+            '<' => moves.push(Move::L),
+            '>' => moves.push(Move::R),
+            '^' => moves.push(Move::U),
+            'v' => moves.push(Move::D),
+            _ => {},
+        }
+    }
+
+    println!("Time taken: {:?}", start.elapsed());
+
+    Ok(())
+}
