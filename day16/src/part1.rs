72395441b139aae9a9a9236e3c581ed70341d7c1
BirdeeHub <birdee@localhost>
2024-12-27 07:11:28 -0800

    Shit code!
    (auto-msg whatthecommit.com)
    
    On branch master
    Your branch is up to date with 'origin/master'.
    
    Changes to be committed:
      (use "git restore --staged <file>..." to unstage)
    	modified:   ../day15/Cargo.toml
    	new file:   Cargo.lock
    	new file:   Cargo.toml
    	new file:   input1
    	new file:   src/main.rs
    	new file:   src/part1.rs
    
    Changes not staged for commit:
      (use "git add <file>..." to update what will be committed)
      (use "git restore <file>..." to discard changes in working directory)
    	modified:   ../day15/Cargo.lock

---
+++ day16/src/part1.rs
@@ -0,0 +1,55 @@
+use std::fs::File;
+use std::time::Instant;
+use std::io::{self, BufRead, BufReader};
+use std::env;
+
+#[derive(Debug,Copy,Clone)]
+enum Space {
+    Empty,
+    Wall,
+}
+
+pub fn run() -> io::Result<()> {
+    let start = Instant::now();
+    let file = File::open(match std::env::args().collect::<Vec<String>>().get(1) {
+        Some(fp) => fp.to_string(),
+        _ => env::var("AOC_INPUT").expect("AOC_INPUT not set")
+    })?;
+    let reader = BufReader::new(file);
+
+    let mut map = Vec::new();
+    let mut start_pos = (0,0);
+    let mut end_pos = (0,0);
+    for (i, line) in reader.lines().enumerate() {
+        let line = line?;
+        let mut row = Vec::new();
+        for (j, c) in line.chars().enumerate() {
+            match c {
+                '#' => {
+                    row.push(Space::Wall);
+                },
+                '.' => {
+                    row.push(Space::Empty);
+                },
+                'S' => {
+                    start_pos = (i,j);
+                    row.push(Space::Empty);
+                },
+                'E' => {
+                    end_pos = (i,j);
+                    row.push(Space::Empty);
+                },
+                _ => {},
+            }
+        }
+        map.push(row);
+    }
+    println!("{:?} {:?}", start_pos, end_pos);
+    for row in &map {
+        println!("{:?}", row);
+    }
+
+    println!("Time taken: {:?}", start.elapsed());
+
+    Ok(())
+}
